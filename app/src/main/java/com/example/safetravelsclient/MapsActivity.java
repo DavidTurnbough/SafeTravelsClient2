2720a0ffecea0852082e0a173c93194018a3a241
William Henness <whenness33@gmail.com>
2020-01-31 11:02:06 -0600

    First commit

--- app/src/main/java/com/example/safetravelsclient/MapsActivity.java
+++ app/src/main/java/com/example/safetravelsclient/MapsActivity.java
@@ -12,7 +12,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+//Henness Comment
     private GoogleMap mMap;
 
     @Override
